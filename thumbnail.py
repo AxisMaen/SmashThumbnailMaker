a96613a70694e490ccdde4152952cca4161d4bd5
AxisMaen <48635776+AxisMaen@users.noreply.github.com>
2019-06-19 21:22:34 -0400

    Add files via upload

--- thumbnail.py
+++ thumbnail.py
@@ -5,25 +5,30 @@
 from gimpfu import *
 
 def do_stuff(img, char1, char2) :
-    gimp.progress_init("Doing stuff to " + layer.name + "...")
+    #gimp.progress_init("Doing stuff to " + layer.name + "...")
 
     # Set up an undo group, so the operation will be undone in one step.
     pdb.gimp_undo_push_group_start(img)
-
-    # Do stuff here.
-    pdb.gimp_invert(layer)
-
+	
+	#make all layers invisible to start
+    for layer in gimp.image_list()[0].layers:
+        layer.opacity = 0.0
+		
+	#if it is the char we want, make it visible
+    char1.opacity = 100.0
+    char2.opacity = 100.0
+	
     # Close the undo group.
     pdb.gimp_undo_push_group_end(img)
 
 register(
     "python_fu_do_stuff",
-    "Do stuff",
-    "Longer description of doing stuff",
-    "Your Name",
-    "Your Name",
-    "2010",
-    "Do Stuff...",
+    "Smash Thumbnail Maker",
+    "A python plugin for gimp that allows for easy creation of thumbnails",
+    "Matthew Rodgers",
+    "Matthew Rodgers",
+    "2019",
+    "Thumbnails...",
     "*",      # Alternately use RGB, RGB*, GRAY*, INDEXED etc.
     [	
 		(PF_IMAGE, "img", "Input image", None),
